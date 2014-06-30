491c14237cd2ae9ce301eafcfce77d67adf06140
Hrishikesh Pathak <hrishp@microsoft.com>
2014-06-30 14:13:42 -0700

    Setting FileVersion VersionStringInfo

--- scripts/GenerateRedisVersion.ps1
+++ scripts/GenerateRedisVersion.ps1
@@ -32,16 +32,21 @@ $RedisServerResourceFile = "msvs\RedisServer.rc"
 $ResourceData = Get-Content $RedisServerResourceFile
 $oldProductVersion = '"ProductVersion", "0.0.0.0"' 
 $newProductVersion = "`"ProductVersion`", `"$($redisVersion).$($buildNumber)_$($versionTag)`""
-$oldFileVersion = 'FILEVERSION 0,0,0,0'
-$newFileVersion = "FILEVERSION $($redisVersionTokens[0]),$($redisVersionTokens[1]),$($redisVersionTokens[2]),$($buildNumber)"
 
-$NewResourceData = $ResourceData -replace $oldFileVersion, $newFileVersion
+$oldFileVersionHeader = 'FILEVERSION 0,0,0,0'
+$newFileVersionHeader = "FILEVERSION $($redisVersionTokens[0]),$($redisVersionTokens[1]),$($redisVersionTokens[2]),$($buildNumber)"
+$oldFileVersion = '"FileVersion", "0.0.0.0"'
+$newFileVersion = "`"FileVersion`", `"$($redisVersion).$($buildNumber)`""
+
+$NewResourceData = $ResourceData -replace $oldFileVersionHeader, $newFileVersionHeader
+$NewResourceData = $NewResourceData -replace $oldFileVersion, $newFileVersion
 $NewResourceData = $NewResourceData -replace $oldProductVersion, $newProductVersion
 Set-Content -path $RedisServerResourceFile  -value $NewResourceData
 
 Write-Host "Done updating RedisServer.rc file..."
-Write-Host "New File Version: " + $newFileVersion
-Write-Host "New Product Version: " + $newProductVersion
+Write-Host "New File Version Header: " $newFileVersionHeader
+Write-Host "New File Version: " $newFileVersion
+Write-Host "New Product Version: " $newProductVersion
 
 
 
