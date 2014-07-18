9999ba83d677679684eee995b4938edbfb3e7a5d
antirez <antirez@gmail.com>
2014-07-18 10:15:51 +0200

    PING: backward compatible error for wrong number of args.

--- src/redis.c
+++ src/redis.c
@@ -2457,7 +2457,8 @@ void authCommand(redisClient *c) {
 void pingCommand(redisClient *c) {
     /* The command takes zero or one arguments. */
     if (c->argc > 2) {
-        addReply(c,shared.syntaxerr);
+        addReplyErrorFormat(c,"wrong number of arguments for '%s' command",
+            c->cmd->name);
         return;
     }
 
